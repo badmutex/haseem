956a034c1c3054842a5ec66e6147f4692483c14f
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-02-10 18:59:28 -0500

    forgot to add the rmsd test module

---
+++ test/RMSD.hs
@@ -0,0 +1,42 @@
+
+import Haseem.Types
+import Haseem.Monad
+import Haseem.Analysis.Config.FaH
+import Haseem.Analysis.VMD.RMSD
+import Haseem.Analysis.TarBz2
+
+import System.FilePath
+
+
+proot = "/home/badi/Research/fah/test/data/PROJ10005"
+fah = MkFaH {
+        projectRoot = Dir proot
+      , run         = 6
+      , clone       = 0
+      , gen         = 0
+      }
+
+
+hcfg = MkHaseemConfig {
+         logger   = undefined
+       , config   = fah
+       , workArea = Dir "/tmp/wa"
+       }
+
+vmdcfg = let root = "/home/badi/Research"
+         in VMDConfig {
+                  vmd_bin      = "vmd"
+                , psfpath      = root </> "md/ww14/folded/ww_structure_14_model_charm.psf"
+                , foldedpath   = root </> "md/ww14/folded/ww_structure_14_model_charm.pdb"
+                , scriptname   = "rmsd.tcl"
+                , resultsname  = "rmsd.txt"
+                , dcdname      = "ww.dcd"
+                , atomselect   = MkAtomSelect "all"
+                , screenoutput = Default
+                }
+
+genparams = genParams vmdcfg
+
+testrmsd = rmsd genparams
+
+test = runHaseem hcfg ((wrapWorkArea . tarbz2) testrmsd)
