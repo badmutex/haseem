2c739773393260c8846a59246a564d6d3bd35810
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-02-10 15:21:35 -0500

    use plain IO instead of as the base monad

--- Haseem/Monad.hs
+++ Haseem/Monad.hs
@@ -9,7 +9,6 @@ import Haseem.Types
 import Control.Monad.Error
 import Control.Monad.Reader
 import Control.Monad.Writer
-import Control.Concurrent.CHP.Monad
 
 class HConfig a where
     hconfig :: a -> Haseem a b
@@ -24,11 +23,11 @@ data HConfig c =>
 
 newtype HConfig c =>
     Haseem c a = MkHaseem {
-                   runH :: ErrorT String (ReaderT (HaseemConfig c) CHP) a
+                   runH :: ErrorT String (ReaderT (HaseemConfig c) IO) a
                  }
 
 
 runHaseem :: HConfig c =>
-             HaseemConfig c -> Haseem c a -> CHP (Either String a)
+             HaseemConfig c -> Haseem c a -> IO (Either String a)
 runHaseem hconfig haseem = flip runReaderT hconfig . runErrorT $ runH haseem
 
