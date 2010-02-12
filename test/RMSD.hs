8bbcaf85f111976d396794703ac5594239775219
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-02-12 11:23:45 -0500

    fip35 rmsd test

--- test/RMSD.hs
+++ test/RMSD.hs
@@ -1,3 +1,5 @@
+#!/bin/env runhaskell
+
 module Main where
 
 import Haseem.Types
@@ -15,14 +17,16 @@ import Text.Printf
 main = do
   [tarball] <- getArgs
   let
-      rmsd' = rmsd genparams
+      rmsd'     = rmsd genparams
       genparams = genParams vmdcfg
 
-      vmdcfg = let root = "~/Research"
+      vmdcfg = let root  = "~/Research/md"
+                   ww14  = root </> "ww14/folded/ww_structure_14_model_charm"
+                   fip35 = root </> "fip35/folded/ww_folded_nowater1"
                in VMDConfig {
                         vmd_bin      = "vmd"
-                      , psfpath      = root </> "md/ww14/folded/ww_structure_14_model_charm.psf"
-                      , foldedpath   = root </> "md/ww14/folded/ww_structure_14_model_charm.pdb"
+                      , psfpath      = fip35 <.> "psf"
+                      , foldedpath   = fip35 <.> "pdb"
                       , scriptname   = "rmsd.tcl"
                       , resultsname  = "rmsd.txt"
                       , dcdname      = "ww.dcd"
@@ -36,9 +40,11 @@ main = do
              , workArea = Dir "/tmp/test/wa"
                }
 
-  (Right rmsds) <- runHaseem hcfg ((wrapWorkArea . myWorkArea . tarbz2) rmsd')
-  let frames = [0..]
-      results = zip frames rmsds
-      ssv :: (Integer,Double) -> String
-      ssv (f,r) = printf "%i %f" f r
-  mapM_ (putStrLn . ssv) results
+  rmsds <- runHaseem hcfg ((wrapWorkArea . myWorkArea . tarbz2) rmsd')
+  case rmsds of
+    Left e -> putStrLn $ "Rmsd calculation failed with: " ++ e
+    Right rs -> do let frames    = [0..]
+                       results   = zip frames rs
+                       ssv       :: (Integer,Double) -> String
+                       ssv (f,r) = printf "%i %f" f r
+                   mapM_ (putStrLn . ssv) results
